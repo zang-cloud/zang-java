85cd46c528ef070eb6a53428bf6140d2725fd42d
Sandesh <schopdekar@avaya.com>
2020-01-29 20:36:39 +0530

    cnamLookup

--- src/main/java/com/zang/api/domain/list/CnamLookupsList.java
+++ src/main/java/com/zang/api/domain/list/CnamLookupsList.java
@@ -3,9 +3,11 @@ package com.zang.api.domain.list;
 import com.zang.api.domain.CnamLookup;
 import com.zang.api.domain.ZangList;
 import org.codehaus.jackson.annotate.JsonProperty;
+import org.codehaus.jackson.annotate.JsonIgnoreProperties;
 
 import java.util.List;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class CnamLookupsList extends ZangList<CnamLookup> {
 
     @Override
